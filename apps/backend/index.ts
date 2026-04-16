97cf2fe487f1677b72c985767e42ced85be55d8f
suphanata <suphanat.a@borntodev.com>
2026-04-16 17:19:07 +0700

    [ROOT, BE] : setup the project

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,1 +1,6 @@
-console.log("Hello via Bun!");
+import { Elysia } from "elysia";
+import { appConfig } from "./config";
+
+const app = new Elysia().get("/", () => "Hello World!").listen(appConfig.port);
+
+console.log(`Server is running on port ${appConfig.port}`);
